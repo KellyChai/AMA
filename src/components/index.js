23aa3fea1d75ae90f6fe62ffb2790267eed287f1
KellyChai <75603539+KellyChai@users.noreply.github.com>
2022-05-09 14:24:53 +0800

    Update index.js
    put password page

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { default as Signin } from "./Signin";
 export { default as SignUp } from "./SignUp";
 // export { default as About } from "./About";
 export { default as Contact } from "./Contact";
+export { default as Password } from "./Password";
